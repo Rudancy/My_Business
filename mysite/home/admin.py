9e8bcde354fe0b7fdcd05a53d6a68028ce1b62f8
Rudancy <45081024+Rudancy@users.noreply.github.com>
2021-04-10 15:29:52 +0100

    added new model, added to admin

--- mysite/home/admin.py
+++ mysite/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import business_site_header
 
 # Register your models here.
+
+admin.site.register(business_site_header)
--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class business_site_header(models.Model):
+    title = models.CharField(max_length=30)
+    descrition = models.TextField(max_length=200)
