1966fdc790451bdfdedc6f043966b7d35c39b5be
Rudancy <45081024+Rudancy@users.noreply.github.com>
2021-04-13 13:54:33 +0100

    added for loop to home html, added view to url, deleted __str__

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -3,26 +3,24 @@ from django.db import models
 # Create your models here.
 
 class home_page(models.Model):
-    masthead = models.CharField(max_length=30, default='')
-    description = models.TextField(max_length=200, default='')
-    icon_1_header = models.CharField(max_length=30, default='')
-    icon_1_description = models.TextField(max_length=30, default='')
-    icon_2_header = models.CharField(max_length=30, default='')
-    icon_2_description = models.TextField(max_length=30, default='')
-    icon_3_header = models.CharField(max_length=30, default='')
-    icon_3_description = models.TextField(max_length=30, default='')
-    showcase_1_header = models.CharField(max_length=30, default='')
-    showcase_1_image = models.ImageField(upload_to='images', default='')
-    showcase_1_description = models.TextField(max_length=100, default='')
-    showcase_2_header = models.CharField(max_length=30, default='')
-    showcase_2_image = models.ImageField(upload_to='images', default='')
-    showcase_2_description = models.TextField(max_length=100, default='')
-    showcase_3_header = models.CharField(max_length=30, default='')
-    showcase_3_image = models.ImageField(upload_to='images', default='')
-    showcase_3_description = models.TextField(max_length=100, default='')
+    masthead = models.CharField(max_length=30, default='', blank=True)
+    description = models.TextField(max_length=200, default='', blank=True)
+    icon_1_header = models.CharField(max_length=30, default='', blank=True)
+    icon_1_description = models.TextField(max_length=30, default='', blank=True)
+    icon_2_header = models.CharField(max_length=30, default='', blank=True)
+    icon_2_description = models.TextField(max_length=30, default='', blank=True)
+    icon_3_header = models.CharField(max_length=30, default='', blank=True)
+    icon_3_description = models.TextField(max_length=30, default='', blank=True)
+    showcase_1_header = models.CharField(max_length=30, default='', blank=True)
+    showcase_1_image = models.ImageField(upload_to='images', default='', blank=True)
+    showcase_1_description = models.TextField(max_length=100, default='', blank=True)
+    showcase_2_header = models.CharField(max_length=30, default='', blank=True)
+    showcase_2_image = models.ImageField(upload_to='images', default='', blank=True)
+    showcase_2_description = models.TextField(max_length=100, default='', blank=True)
+    showcase_3_header = models.CharField(max_length=30, default='', blank=True)
+    showcase_3_image = models.ImageField(upload_to='images', default='', blank=True)
+    showcase_3_description = models.TextField(max_length=100, default='', blank=True)
     
     
     def __str__(self):
-        return self.masthead, self.description, self.icon_1_header, self.icon_1_description, self.icon_2_header, self.icon_2_description, 
-        self.icon_3_header, self.icon_3_description, self.showcase_1_header, self.showcase_1_image, self.showcase_1_description, self.showcase_2_header, 
-        self.showcase_2_image, self.showcase_2_description, self.showcase_3_header, self.showcase_3_image, self.showcase_3_description 
+        return self.masthead
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, HttpResponse
 from .models import home_page
 
 # Create your views here.
-def index(request):
+def home_page_values(request):
 
     home_page_values = home_page.objects.all()
 
-    return render(request, 'home.html', {"home": home_page_values})
+    return render(request, 'home.html', {"home_page_values": home_page_values})
